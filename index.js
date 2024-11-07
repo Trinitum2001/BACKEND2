ae2e0afdb39362609285f124f8e8da35b4cd1630
Ruben Jose Quintero Sala <145045705+Trinitum2001@users.noreply.github.com>
2024-11-07 16:58:18 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -103,8 +103,7 @@ app.put('/api/notes/:id', (req, res) => {
 });
 
 
-const port = 3001;
-
+const PORT= process.env.PORT || 3001;
 app.listen(port, () => {
 console.log('Server running on port ' + port);
 
